a60b9de71699bbfa32c850bb6d173e803ddced24
tienhuynh150699@gmail.com <tienhuynh150699@gmail.com>
2019-05-27 15:26:41 +0700

    MyBrowser

---
+++ MyBrowser/app/src/main/java/com/example/mybrowser/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.mybrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public Button btnGo;
+    public Button btnBack;
+    public Button btnForward;
+    public EditText inputUrl;
+    public WebView mainView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mainView = (WebView) findViewById(R.id.mainView);
+        mainView.setWebViewClient(new MyBrowser());
+        btnGo = (Button) findViewById(R.id.btnGo);
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showUrl();
+            }
+        });
+    }
+    private String getinputUrl()
+    {
+     inputUrl = (EditText) findViewById(R.id.txtinputUrl);
+     String url;
+     url = inputUrl.getText().toString();
+     return url;
+    }
+    private void showUrl()
+        {
+            String url;
+            url = getinputUrl();
+            mainView.getSettings().setLoadsImagesAutomatically(true);
+            mainView.getSettings().setJavaScriptEnabled(true);
+            mainView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+            mainView.loadUrl(url);
+        }
+
+    private class MyBrowser extends WebViewClient
+        {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String Url)
+            {
+                view.loadUrl(Url);
+                inputUrl.setText(Url);
+                return true;
+            }
+        }
+
+
+}
